0cfc91d11a2f4fed6234c212c8ce610214ffe072
AatreshKulkarni <aatresh.kulkarni08@gmail.com>
2018-04-07 17:16:51 +0530

    Operators in Python

---
+++ ex2_op.py
@@ -0,0 +1,20 @@
+# Basic Operators in python
+
+# Variable declaration
+a = 10
+b = 5
+
+# Sum of a and b
+print(a + b)
+
+# Difference of a and b
+print(a - b)
+
+# Multiplication of a and b
+print(a * b)
+
+# Division of a and b
+print(a / b)
+
+# Modulus operation on a and b
+print(a % b) 
