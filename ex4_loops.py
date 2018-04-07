f2d481a813ff35a8b831700ee27c99e4865bdaae
AatreshKulkarni <aatresh.kulkarni08@gmail.com>
2018-04-07 20:35:58 +0530

    Loops in Python

---
+++ ex4_loops.py
@@ -0,0 +1,22 @@
+# Loops in Python 1.for, 2.while
+
+# for loop example
+
+# Defining a list
+days_of_week = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+
+# Printing contents of the list
+print("Days of the week")
+for days in days_of_week:
+	print(days) 
+
+	
+# while loop example	
+
+# Defining a variable by name x
+x = 0
+
+# printing 0 to 10 
+while x <= 10:
+	print("Current value of x: ",x)
+	x += 1 # reassigning x to x by adding 1 to it
