ab417872da86114836e9b562312133641c1b84a5
AatreshKulkarni <aatresh.kulkarni08@gmail.com>
2018-04-07 15:52:01 +0530

    Variables types in python

---
+++ ex1_var.py
@@ -0,0 +1,35 @@
+# THE BASIC DATA TYPES OF PYTHON
+
+#Variable Assignment
+a = 10
+b = 10.2
+c = True
+d = [12, 'a', True, '10.2']
+e = (10, 20, 30)
+f = "Python"
+g = {'apple' : 10, 'banana' : 12, 'mango' : 15}
+
+# print(): This function  prints  onto the output screem
+
+# type() This function helps to know the type of the variable
+
+# Integer
+print(type(a))
+
+# Float
+print(type(b))
+
+# boolean
+type(c)
+
+# list
+print(type(d))
+
+# tuple
+print(type(e))
+
+# string
+print(type(f))
+
+# dictionary
+print(type(g))
