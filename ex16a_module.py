969d3c387755fdacd2594969a743d17d00ebe4b8
AatreshKulkarni <aatresh.kulkarni08@gmail.com>
2018-04-09 18:25:57 +0530

    This is the module

---
+++ ex16a_module.py
@@ -0,0 +1,12 @@
+# Every file with .py extension in python is a module
+# Defining a function to to find even or odd
+def isEven(num):
+	# Even
+	if num % 2 == 0:
+		print(num," is even")
+	#Odd
+	else:
+		print(num," is odd")
+		
+		
+isEven(11)
