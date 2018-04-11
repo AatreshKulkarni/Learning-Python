c416821f4645802991e41ceccfdb89bfdb240db0
AatreshKulkarni <aatresh.kulkarni08@gmail.com>
2018-04-11 13:22:16 +0530

    Updated comments in Prime1

--- basic_programs/pgm12_prime1.py
+++ basic_programs/pgm12_prime1.py
@@ -10,11 +10,15 @@ def isPrime(num):
 		print("Not a prime number")
 	# Here Actual prime number logic goes
 	else:
+		# Difining a count variable initialized to zero
 		count = 0
+		# Iterate from 3 to num with step size 2(step size 2:eleminates even number)
 		for i in range(3,num + 1,2):
+			# If num evenly divisible by any value then increment count by 1
 			if num % i == 0:
 				count+=1
 	
+		# After all iteration if count = 1, itmeans num has only 1 and itself as its factors
 		if count == 1:
 			print("Prime number")
 		else:
